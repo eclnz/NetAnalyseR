243c369e2f2e085ca48beb0e262d35c2f4deb8b2
eclnz <edward.clarkie@gmail.com>
2024-04-02 21:45:28 +1300

    Dramatically Improved Speed of `shortest_distance()` Using Floyd Warshall Algorithm
    The original `shortest_distance()` function iterated over each combination of each
    node in the network to determine the individual distance between each node. This
    approach is inefficient which is noticible when the network sizes are very large
    and or the networks have a high density. This inefficiency severely impacts the
    performance of `local_efficiency_wei()` which calls `shortest_distance()` for each
    neighborhood within a given network.
    
    The Floyd Warshall Algorithm is implemented in C++ which is called from R through
    Rcpp. This aims to increase speed further but it is unknown whether a simple
    application of the Floyd Warshall Algorithm within R would have similar speed
    benefits.
    
    This may pave the way for the calculation of other global metrics to be executed
    in C++.

---
+++ src/floydWarshallRcpp.cpp
@@ -0,0 +1,50 @@
+//' @name floydWarshallRcpp
+//' @title Floyd-Warshall Algorithm in R
+//' @description This function implements the Floyd-Warshall algorithm using Rcpp for finding the shortest distances
+//' between every pair of vertices in a given edge weighted directed Graph. It operates on a matrix representation of the graph.
+//' @param inputMatrix A NumericMatrix representing the adjacency matrix of the graph where the element at the ith row
+//' and jth column represents the weight of the edge from vertex i to vertex j. If there is no edge between vertex i and vertex j,
+//' the value should be 0 for i != j.
+//' @return A NumericMatrix where the element at the ith row and jth column represents the shortest distance from vertex i
+//' to vertex j in the input graph.
+//' @examples
+//' # Define the adjacency matrix for a graph
+//' W <- matrix(c(0, 2, 1, 4, 2, 0, 3, 5, 1, 3, 0, 6, 4, 5, 6, 0), nrow = 4, byrow = TRUE)
+//' # Calculate shortest paths
+//' shortestPaths <- floydWarshallRcpp(W)
+//' print(shortestPaths)
+//' @export
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericMatrix floydWarshallRcpp(NumericMatrix inputMatrix) {
+  int numVertices = inputMatrix.nrow(); // Assuming a square matrix
+  NumericMatrix distanceMatrix(numVertices, numVertices);
+
+  // Initialize the distance matrix
+  for (int i = 0; i < numVertices; ++i) {
+    for (int j = 0; j < numVertices; ++j) {
+      if (inputMatrix(i, j) == 0 && i != j) {
+        // Use R's representation of infinity for unreachable vertices
+        distanceMatrix(i, j) = R_PosInf;
+      } else {
+        distanceMatrix(i, j) = inputMatrix(i, j);
+      }
+    }
+  }
+
+  // Apply Floyd-Warshall algorithm
+  for (int k = 0; k < numVertices; ++k) {
+    for (int i = 0; i < numVertices; ++i) {
+      for (int j = 0; j < numVertices; ++j) {
+        if (distanceMatrix(i, k) + distanceMatrix(k, j) < distanceMatrix(i, j)) {
+          distanceMatrix(i, j) = distanceMatrix(i, k) + distanceMatrix(k, j);
+        }
+      }
+    }
+  }
+
+  return distanceMatrix;
+}
